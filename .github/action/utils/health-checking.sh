8625d76a65d220e3ddd712d69037b164de433c9c
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-06 17:51:03 +0200

    Create health-checking.sh

---
+++ .github/action/utils/health-checking.sh
@@ -0,0 +1,29 @@
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "Health Checking..."
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo ALB_ARN: ${{ env.ALB_ARN }}
+sleep 30
+echo "1st instance state:"
+HEALTH_STATUS=0
+while [ ${HEALTH_STATUS} == 0 ];
+do 
+  aws elbv2 describe-target-health --target-group-arn ${{ env.ALB_ARN }} --query 'TargetHealthDescriptions[*].[Target.Id, TargetHealth.State]' --output json | grep draining || HEALTH_STATUS=$?
+  sleep 10
+done
+  HEALTH_STATUS=0
+while [ ${HEALTH_STATUS} == 0 ];
+do 
+  aws elbv2 describe-target-health --target-group-arn ${{ env.ALB_ARN }} --query 'TargetHealthDescriptions[*].[Target.Id, TargetHealth.State]' --output json | grep initial || HEALTH_STATUS=$?
+  sleep 10
+done
+  HEALTH_STATUS=0
+  aws elbv2 describe-target-health --target-group-arn ${{ env.ALB_ARN }} --query 'TargetHealthDescriptions[*].[Target.Id, TargetHealth.State]' --output json | grep unhealthy || HEALTH_STATUS=$?
+if [  ${HEALTH_STATUS} == 0 ]
+then
+  echo "Unhealthy"
+  echo "Deployment failed"
+exit 1
+else
+  echo "Healthy"
+  echo "Deployment Completed"
+fi
