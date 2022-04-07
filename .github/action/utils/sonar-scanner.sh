4bfdafe27240d7626f64a644e846f879574d4edb
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-07 09:27:26 +0200

    Update sonar-scanner.sh

--- .github/action/utils/sonar-scanner.sh
+++ .github/action/utils/sonar-scanner.sh
@@ -1,4 +1,6 @@
-
+echo "***************************************************"
+echo "Sonar scanner started..."
+echo "***************************************************"
 sonar-scanner
   -Dsonar.login=${{ env.SONAR_USER }}
   -Dsonar.password=${{ env.SONAR_PASS }}
@@ -6,3 +8,6 @@ sonar-scanner
   -Dsonar.projectKey=${{ env.SONAR_PROJECT_ID }}
   -Dsonar.java.binaries=**/target/classes
   -DDsonar.language=java
+echo "***************************************************"
+echo "Sonar scanner complete..."
+echo "***************************************************"
