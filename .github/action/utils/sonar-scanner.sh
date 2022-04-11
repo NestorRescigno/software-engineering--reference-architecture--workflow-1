733ecf58b3acadd80e633f03b30a85ee98020b0a
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-11 14:26:27 +0200

    conditional in script
    if url isn't empty then allow sonar for scanner code. simple control disable sonar

--- .github/action/utils/sonar-scanner.sh
+++ .github/action/utils/sonar-scanner.sh
@@ -2,16 +2,31 @@
 # *************           IBERIA L.A.E.                   *************
 # *************       by software Engineering             *************
 # *********************************************************************
-echo "***************************************************"
-echo "Sonar scanner started..."
-echo "***************************************************"
-sonar-scanner
-  -Dsonar.login=${{ env.SONAR_USER }}
-  -Dsonar.password=${{ env.SONAR_PASS }}
-  -Dsonar.host.url=${{ env.SONAR_URL }}
-  -Dsonar.projectKey=${{ env.SONAR_PROJECT_ID }}
-  -Dsonar.java.binaries=**/target/classes
-  -DDsonar.language=java
-echo "***************************************************"
-echo "Sonar scanner complete..."
-echo "***************************************************"
+# Setting variable
+SONAR_URL = $1
+SONAR_USER = $2
+SONAR_PASS = $3
+SONAR_PROJECT_ID = $4
+SONAR_TARGET_PATH = $5
+SONAR_LANGUAGE = $6
+# if url isn't empty then allow sonar for scanner code
+if [ SONAR_URL != ""]
+then  
+  echo "***************************************************"
+  echo "Sonar scanner started..."
+  echo "***************************************************"
+  sonar-scanner
+    -Dsonar.login=${SONAR_USER}
+    -Dsonar.password=${SONAR_PASS}
+    -Dsonar.host.url=${SONAR_URL}
+    -Dsonar.projectKey=${SONAR_PROJECT_ID}
+    -Dsonar.java.binaries={SONAR_TARGET_PATH}
+    -DDsonar.language=${SONAR_LANGUAGE}
+  echo "***************************************************"
+  echo "Sonar scanner complete..."
+  echo "***************************************************"
+else
+  echo "***************************************************"
+  echo "Sonar scanner is disable..."
+  echo "***************************************************"
+fi
