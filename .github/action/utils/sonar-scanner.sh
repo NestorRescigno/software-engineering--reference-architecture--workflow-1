d31cc307a4bb11106b1c26a040bb86be9b4c3efa
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-11 15:23:11 +0200

    temporal update script

--- .github/action/utils/sonar-scanner.sh
+++ .github/action/utils/sonar-scanner.sh
@@ -7,7 +7,6 @@ SONAR_URL = $1
 SONAR_USER = $2
 SONAR_PASS = $3
 SONAR_PROJECT_ID = $4
-SONAR_TARGET_PATH = $5
 SONAR_LANGUAGE = $6
 # if url isn't empty then allow sonar for scanner code
 if [ ${SONAR_URL} != ""]
@@ -15,13 +14,31 @@ then
   echo "***************************************************"
   echo "Sonar scanner started..."
   echo "***************************************************"
-  sonar-scanner
-    -Dsonar.login=${SONAR_USER}
-    -Dsonar.password=${SONAR_PASS}
-    -Dsonar.host.url=${SONAR_URL}
-    -Dsonar.projectKey=${SONAR_PROJECT_ID}
-    -Dsonar.java.binaries={SONAR_TARGET_PATH}
-    -DDsonar.language=${SONAR_LANGUAGE}
+  if[${SONAR_LANGUAGE}="java"]
+  then
+    # sonar scannar java setup
+    sonar-scanner
+      -Dsonar.login=${SONAR_USER}
+      -Dsonar.password=${SONAR_PASS}
+      -Dsonar.host.url=${SONAR_URL}
+      -Dsonar.projectKey=${SONAR_PROJECT_ID}
+      -Dsonar.java.binaries=**/target/classes
+      -DDsonar.language=java
+  elif[${SONAR_LANGUAGE}="angular"]
+  then
+    # sonar scannar angular setup
+    sonar-scanner
+      -Dsonar.login=${SONAR_USER}
+      -Dsonar.password=${SONAR_PASS}
+      -Dsonar.host.url=${SONAR_URL}
+      -Dsonar.projectKey=${SONAR_PROJECT_ID}
+      -Dsonar.projectName=${SONAR_PROJECT_ID}
+      -Dsonar.sourceEncoding=UTF-8
+      -Dsonar.exclusions=**/node_modules/**
+      -Dsonar.tests=src
+      -Dsonar.test.inclusions=**/*.spec.ts
+      -Dsonar.typescript.lcov.reportPaths=coverage/lcov.info
+  fi
   echo "***************************************************"
   echo "Sonar scanner complete..."
   echo "***************************************************"
