1515ea9775d4aa23bf78b37b5e4966bf39b772f9
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-11 16:37:40 +0200

    Update sonar-scanner.sh

--- .github/action/utils/sonar-scanner.sh
+++ .github/action/utils/sonar-scanner.sh
@@ -6,8 +6,8 @@
 SONAR_URL = $1
 SONAR_USER = $2
 SONAR_PASS = $3
-SONAR_PROJECT_ID = $4
-SONAR_LANGUAGE = $6
+SONAR_LANGUAGE = $4
+workspace = $5
 # if url isn't empty then allow sonar for scanner code
 if [ ${SONAR_URL} != ""]
 then  
@@ -16,16 +16,31 @@ then
   echo "***************************************************"
   if [${SONAR_LANGUAGE}="java"]
   then
+    # get information from pom
+    groupId=$(sed -n 's,.*<groupId>\(.*\)</groupId>.*,\1,p' ${ workspace }/pom.xml | head -1)
+    artifactId=$(sed -n 's,.*<artifactId>\(.*\)</artifactId>.*,\1,p' ${ workspace }/pom.xml | head -1)
+    version=$(sed -n 's,.*<version>\(.*\)</version>.*,\1,p' ${ workspace }/pom.xml | head -1)
+    SONAR_PROJECT_ID=${groupId}+":"+${artifactId}
+    SONAR_VERSION_ID = ${version}
+    
     # sonar scannar java setup
     sonar-scanner
       -Dsonar.login=${SONAR_USER}
       -Dsonar.password=${SONAR_PASS}
       -Dsonar.host.url=${SONAR_URL}
       -Dsonar.projectKey=${SONAR_PROJECT_ID}
+      -Dsonar.projectName=${SONAR_PROJECT_ID}
+      -Dsonar.projectVersion=${SONAR_VERSION_ID}
       -Dsonar.java.binaries=**/target/classes
       -Dsonar.language=java
   elif [${SONAR_LANGUAGE}="angular"]
   then
+    # get information from package.json
+    name=
+    version=
+    SONAR_PROJECT_ID = ${name}
+    SONAR_VERSION_ID = ${version}
+    
     # sonar scannar angular setup
     sonar-scanner
       -Dsonar.login=${SONAR_USER}
@@ -33,6 +48,7 @@ then
       -Dsonar.host.url=${SONAR_URL}
       -Dsonar.projectKey=${SONAR_PROJECT_ID}
       -Dsonar.projectName=${SONAR_PROJECT_ID}
+      -Dsonar.projectVersion=${SONAR_VERSION_ID}
       -Dsonar.sourceEncoding=UTF-8
       -Dsonar.exclusions=**/node_modules/**
       -Dsonar.tests=src
