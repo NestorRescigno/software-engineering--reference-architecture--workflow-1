cd76fe7f0687bc5a7bb70639427d760bc37828a5
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-12 09:57:51 +0200

    Update nexus-registry.sh

--- .github/action/utils/nexus-registry.sh
+++ .github/action/utils/nexus-registry.sh
@@ -5,7 +5,62 @@
 echo "***************************************************"
 echo "Registy artifact to nexus repository"
 echo "***************************************************"
-echo "NO IMPLEMENT"
-echo "***************************************************"
-echo "Registy complete"
-echo "***************************************************"
+
+# setting variable
+REPOSITORY_URL=%1
+LANGUAGE=%2
+ref = %3
+workspace=%4
+
+if [${REPOSITORY_URL} != ""] 
+then
+    if [${LANGUAGE} = "java"]
+    then
+    echo "***************************************************"
+    echo "artifact type java"
+    echo "***************************************************"
+      if [ ${{ startsWith(${ ref }, 'refs/heads/develop') }} = true ]
+      then
+        echo "***************************************************"
+        echo "upload snapshop"
+        echo "***************************************************"
+        SNAPSHOTS_REPOSITORY_URL = ${ REPOSITORY_URL } + "/repository/snapshots/"
+        mvn clean deploy -Dmaven.wagon.http.ssl.insecure=true -DaltSnapshotDeploymentRepository=ibis-snapshots::default::${ SNAPSHOTS_REPOSITORY_URL } 
+        --batch-mode
+      elif [${{ startsWith(${ ref }, 'refs/heads/main') }} = true ]
+        echo "***************************************************"
+        echo "upload release"
+        echo "***************************************************"
+        RELEASE_REPOSITORY_URL = ${ REPOSITORY_URL } + "/repository/releases/"
+        mvn clean deploy -Dmaven.wagon.http.ssl.insecure=true -DaltSnapshotDeploymentRepository=ibis-release::default::${ RELEASE_REPOSITORY_URL } 
+        --batch-mode
+      fi
+    else
+      echo "***************************************************"
+      echo "artifact type angular"
+      echo "***************************************************"
+      if [ ${{ startsWith(${ ref }, 'refs/heads/develop') }} = true ]
+      then
+        echo "***************************************************"
+        echo "upload snapshop"
+        echo "***************************************************"
+        
+         # pending implement deploy to repository
+         
+      elif [${{ startsWith(${ ref }, 'refs/heads/main') }} = true ]
+        echo "***************************************************"
+        echo "upload release"
+        echo "***************************************************"
+        
+        # pending implement deploy to repository
+        
+      fi
+    fi
+    echo "***************************************************"
+    echo "Registy complete"
+    echo "***************************************************"
+else
+  echo "***************************************************"
+  echo "Registy disable"
+  echo "***************************************************"
+fi  
