0c8d64597191203210765607e588748f1de37143
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-12 15:39:10 +0200

    Update data.tf

--- module/terrafom/data.tf
+++ module/terrafom/data.tf
@@ -1,3 +1,9 @@
+# *********************************************************************
+# *************           IBERIA L.A.E.                   *************
+# *************       by software Engineering             *************
+# *********************************************************************
+# example code of [data-source](https://www.terraform.io/language/data-sources)
+
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
