2e3ca7ef44224363b704041068d5248df6170a3f
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-12 18:20:23 +0200

    Update terraform-deploy.sh

--- .github/action/utils/terraform-deploy.sh
+++ .github/action/utils/terraform-deploy.sh
@@ -2,23 +2,45 @@
 # *************           IBERIA L.A.E.                   *************
 # *************       by software Engineering             *************
 # *********************************************************************
+
+# setting variable
+AMI_VERSION=%1
+PROJECT=%2
+ENVIROMENT=%3
+AMI_ID=%4
+PREFIX=%5
+cd ${{ github.workspace }}/module/terraform/
+
 echo "***************************************************"
 echo "Deploying with terraform..."
 echo "***************************************************"
-cd ${{ github.workspace }}/module/terraform/
+
+# init terraform module
 terraform init
+
+# create plan terrafom
 terraform plan 
--var "anc_ins_version=${{ needs.init.outputs.AMI_VERSION }}" 
--var "project=ancill" 
--var "environment=int" 
--var "anc_ins_ami_id=${{ env.IMAGE_ID }}" 
--var "environment_prefix=ancill-int"
+-var "version=${AMI_VERSION}" 
+-var "project=${PROJECT}" 
+-var "environment=${ENVIROMENT}" 
+-var "ami_id=${AMI_ID}" 
+-var "environment_prefix=${PREFIX}"
+
+# apply plan terrafom
 terraform apply -auto-approve 
--var "anc_ins_version=${{ needs.init.outputs.AMI_VERSION }}" 
--var "project=ancill" 
--var "environment=int" 
--var "anc_ins_ami_id=${{ env.IMAGE_ID }}" 
--var "environment_prefix=ancill-int"
+-var "version=${AMI_VERSION}" 
+-var "project=${PROJECT}" 
+-var "environment=${ENVIROMENT}" 
+-var "ami_id=${AMI_ID}" 
+-var "environment_prefix=${PREFIX}"
 echo "***************************************************"
 echo "Deploying end..."
 echo "***************************************************"
+
+# *********************************************************************
+# *************           NOTE FOR DEVELOPER              *************
+# *************  the module terrafom is location in       *************
+# *************     ./module/terraform/                   *************
+# *************  define vpn, subnet, and other            *************
+# *************  firt time                                *************
+# *********************************************************************
