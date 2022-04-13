4476eaf80746abdaab507f7077875ad3e5cd4fa0
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-13 18:42:40 +0200

    Update get-tools.sh

--- .github/action/utils/get-tools.sh
+++ .github/action/utils/get-tools.sh
@@ -2,18 +2,22 @@
 # *************           IBERIA L.A.E.                   *************
 # *************       by Software Engineering             *************
 # *********************************************************************
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "Getting SoapUI 5.6.0..."
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
-wget http://s3.amazonaws.com/downloads.eviware/soapuios/5.6.0/SoapUI-5.6.0-linux-bin.tar.gz -O ${{ github.workspace }}/.github/SoapUI-5.6.0-linux-bin.tar.gz
-cd ${{ github.workspace }}/.github
-tar -zxvf SoapUI-5.6.0-linux-bin.tar.gz
+ACTIVE = %1
 
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "Getting sonar cli..."
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+if [${ ACTIVE }=true] then
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo "Getting SoapUI 5.6.0..."
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+  wget http://s3.amazonaws.com/downloads.eviware/soapuios/5.6.0/SoapUI-5.6.0-linux-bin.tar.gz -O ${{ github.workspace }}/.github/SoapUI-5.6.0-linux-bin.tar.gz
+  cd ${{ github.workspace }}/.github
+  tar -zxvf SoapUI-5.6.0-linux-bin.tar.gz
+
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo "Getting sonar cli..."
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "Getting jp..."
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+  echo "Getting jp..."
+  echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+if
