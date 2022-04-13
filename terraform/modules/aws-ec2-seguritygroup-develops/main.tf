3fa56a657f47390fd1d191d869d217bc93fca436
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-13 11:35:14 +0200

    Update main.tf

--- terraform/modules/aws-ec2-seguritygroup-develops/main.tf
+++ terraform/modules/aws-ec2-seguritygroup-develops/main.tf
@@ -1,1 +1,53 @@
+# *********************************************************************
+# *************           IBERIA L.A.E.                   *************
+# *************       by software Engineering             *************
+# *********************************************************************
 
+#####################################
+### New resources ALB SG 
+######################################
+resource "aws_security_group" "anc-alb" {
+  name        = "${var.service_name}-alb-${var.environment_prefix}-sg"
+  description = "SG for ${var.service_name} cluster ALB"
+  vpc_id      = data.aws_vpc.vpc-ancill.id
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [data.aws_security_group.sg-svc-instances.id]
+    description     = "From ${var.service_name} ALB"
+  }
+  tags = merge(
+    local.global_common_tags,
+    tomap({
+      Name = "${var.service_name}-alb-${var.environment_prefix}-sg"
+    })
+  )
+}
+
+#####################################
+### New resources ALB SG Instances 
+######################################
+resource "aws_security_group" "anc-instances" {
+  depends_on = [
+    aws_security_group.anc-alb
+  ]
+  name        = "${var.service_name}-instances-${var.environment_prefix}-sg"
+  description = "SG for ${var.service_name} cluster instances"
+  vpc_id      = data.aws_vpc.vpc-ancill.id
+
+  ingress {
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.anc-alb.id]
+    description = "From ${var.service_name} ALB"
+  }
+
+  tags = merge(
+    local.global_common_tags,
+    tomap({
+      Name = "${var.service_name}-instances-${var.environment_prefix}-sg"
+    })
+  )
+}
