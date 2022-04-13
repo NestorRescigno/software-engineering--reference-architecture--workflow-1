3421dc40ffc2a95210251f64846310064de72321
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-13 15:22:28 +0200

    Update variables.tf

--- terraform/modules/aws-ec2-vpc-iberia/variables.tf
+++ terraform/modules/aws-ec2-vpc-iberia/variables.tf
@@ -51,3 +51,28 @@ variable "global_dns" {
   type        = string
   default     = "cloud.iberia.local"
 }
+
+variable "bucket_name" {
+  description = "bucket_name."
+  type        = string
+}
+
+variable "bucket_key" {
+  description = "bucket_key."
+  type        = string
+}
+
+variable "dynamodb_table" {
+  description = "dynamo db table."
+  type        = string
+}
+
+variable "kms_key_id" {
+  description = "kms key id."
+  type        = string
+}
+
+variable "role_arn" {
+  description = "role aim arn."
+  type        = string
+}
