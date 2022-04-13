95974095f992d683c96472b80253c4cb91d07ef2
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-13 15:28:33 +0200

    Create provider.tf

---
+++ terraform/modules/aws-ec2-vpc-iberia/provider.tf
@@ -0,0 +1,34 @@
+# *********************************************************************
+# *************           IBERIA L.A.E.                   *************
+# *************       by Software Engineering             *************
+# *********************************************************************
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws"
+    version = "3.61.0" }
+  }
+}
+
+####################################################################
+## Change value key service_name with same value in terraform.tfvars
+####################################################################
+
+terraform {
+  backend "s3" {
+   
+    bucket = ${var.bucket_name}  # bucket = "terraform-backend-ancill-accounts"
+    key = ${var.bucket_key}   # key= "prelive/terraform.[service_name].tfstate" 
+
+    region = "eu-central-1"
+
+    dynamodb_table = ${var.dynamodb_table}
+    encrypt        = true
+    kms_key_id     = ${var.kms_key_id}
+    role_arn       = ${var.role_arn}
+  }
+}
