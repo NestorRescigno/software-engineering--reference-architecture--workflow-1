031fcce9eb6c3a5aa72012b9358f494c9df7535c
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-13 14:17:27 +0200

    Update and rename terraform/modules/aws-ec2-vpc-develops/variables.tf to terraform/modules/aws-ec2-vpc-iberia/variables.tf

---
+++ terraform/modules/aws-ec2-vpc-iberia/variables.tf
@@ -0,0 +1,47 @@
+# *********************************************************************
+# *************           IBERIA L.A.E.                   *************
+# *************       by Software Engineering             *************
+# *********************************************************************
+# 000 - Global variables
+
+variable "ami_id" {
+  description = "AMI id"
+  type        = string
+}
+
+variable "version" {
+  description = "Version ID"
+  type        = string
+}
+
+variable "service_name" {
+  description = "The name of the service to be created"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "Region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "project" {
+  description = "Project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+
+variable "environment_prefix" {
+  description = "Environment Prefix."
+  type        = string
+}
+
+variable "global_dns" {
+  description = "dns."
+  type        = string
+  default     = "cloud.iberia.local"
+}
