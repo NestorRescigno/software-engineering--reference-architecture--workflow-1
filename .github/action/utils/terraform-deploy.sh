e91315b3e111f53899922d7f44575e1d687a82cc
NestorRescigno <nestor.rescigno@accenture.com>
2022-04-18 12:31:21 +0200

    add deploy to shell

--- .github/action/utils/terraform-deploy.sh
+++ .github/action/utils/terraform-deploy.sh
@@ -35,6 +35,31 @@ terraform apply -auto-approve
 -var "environment=${ENVIROMENT}" 
 -var "environment_prefix=${PREFIX}"
 
+cd ${{ github.workspace }}/terraform/module/aws-ec2-depoly-iberia
+
+echo "***************************************************"
+echo "Deploying with terraform..."
+echo "***************************************************"
+
+# init terraform module
+terraform init
+
+# create plan terrafom
+terraform plan 
+-var "version=${AMI_VERSION}"
+-var "ami_id=${AMI_ID}" 
+-var "project=${PROJECT}" 
+-var "environment=${ENVIROMENT}" 
+-var "environment_prefix=${PREFIX}"
+
+# apply plan terrafom
+terraform apply -auto-approve
+-var "ami_id=${AMI_ID}" 
+-var "version=${AMI_VERSION}" 
+-var "project=${PROJECT}" 
+-var "environment=${ENVIROMENT}" 
+-var "environment_prefix=${PREFIX}"
+
 # set terrafom arn aws target group output to environment
 echo "aws_alb_target_group_arn=$(terraform output aws_alb_target_group_arn)" >> $GITHUB_ENV  #test in shell or move to run action
 
