c7895f78aeae92e99ff07be2d7396e9beab27021
NestorRescigno <nestor.rescigno@accenture.com>
2022-04-18 09:32:41 +0200

    create image with terraform

--- .github/action/utils/build-image.sh
+++ .github/action/utils/build-image.sh
@@ -2,10 +2,42 @@
 # *************           IBERIA L.A.E.                   *************
 # *************       by software Engineering             *************
 # *********************************************************************
+user_name           = %1
+user_departament    = %2
+instance_type       = %3   
+artifact            = %4
+lenguage            = %5
+
 echo "***************************************************"
 echo "Creating image"
 echo "***************************************************"
-echo "NO IMPLEMENT"
+
+cd ${{ github.workspace }}/terraform/module/aws-ec2-image-iberia
+
+echo "***************************************************"
+echo "Deploying with terraform..."
+echo "***************************************************"
+
+# init terraform module
+terraform init
+
+# create plan terrafom
+terraform plan 
+-var "user_name=${user_name}"
+-var "user_departament=${user_departament}" 
+-var "instance_type=${instance_type}" 
+-var "ref=${artifact}" 
+-var "lenguage_code=${lenguage}"
+
+# apply plan terrafom
+terraform apply -auto-approve
+-var "user_name=${user_name}"
+-var "user_departament=${user_departament}" 
+-var "instance_type=${instance_type}" 
+-var "ref=${artifact}" 
+-var "lenguage_code=${lenguage}"
+
+
 echo "***************************************************"
 echo "Created image"
 echo "***************************************************"
