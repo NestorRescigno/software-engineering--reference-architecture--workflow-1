d69a433657ba40b82c514df6c2792e0e75804b97
NestorDevOps <nrescign@outlook.com>
2022-04-20 16:32:22 +0200

    set var in shell

--- .github/action/utils/get-tools.sh
+++ .github/action/utils/get-tools.sh
@@ -3,7 +3,7 @@
 # *************       by Software Engineering             *************
 # *********************************************************************
 ACTIVE = %1
-
+WORKSPACE=%2
 
 # client aws download without action github
 # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -16,8 +16,8 @@ if [${ ACTIVE }==true] then
   echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
   echo "Getting SoapUI 5.6.0..."
   echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
-  wget http://s3.amazonaws.com/downloads.eviware/soapuios/5.6.0/SoapUI-5.6.0-linux-bin.tar.gz -O ${{ github.workspace }}/.github/SoapUI-5.6.0-linux-bin.tar.gz
-  cd ${{ github.workspace }}/.github
+  wget http://s3.amazonaws.com/downloads.eviware/soapuios/5.6.0/SoapUI-5.6.0-linux-bin.tar.gz -O ${WORKSPACE}/.github/SoapUI-5.6.0-linux-bin.tar.gz
+  cd ${WORKSPACE}/.github
   tar -zxvf SoapUI-5.6.0-linux-bin.tar.gz
 
   echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
