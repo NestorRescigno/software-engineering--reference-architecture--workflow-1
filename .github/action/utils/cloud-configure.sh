1c3df30bf02c944fbc97b5d3970383ff337d3117
NestorDevOps <nrescign@outlook.com>
2022-04-20 17:53:52 +0200

    create install cloud aws and terraform, testrunner

---
+++ .github/action/utils/cloud-configure.sh
@@ -0,0 +1,71 @@
+# *********************************************************************
+# *************           IBERIA L.A.E.                   *************
+# *************       by software Engineering             *************
+# *********************************************************************
+# ************* Type cloud  [azure, aws, google]          *************
+# *********************************************************************
+echo "***************************************************"
+echo " configure cloud credentials..."
+echo "***************************************************"
+TYPE=%1
+ID_KEY_ACCESSS=%2
+ID_KEY_SECRET=%3
+
+echo "***************************************************"
+echo " configure terraform                               "
+echo "***************************************************"
+
+terraform -v
+if echo $? = 128 ; then
+    echo "The program 'terraform' is currently not installed."
+    echo "install program 'terraform'..."
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+    sudo apt-get update && sudo apt-get install terraform
+    terraform -v
+else
+    echo "Continuing with terraform."
+fi
+
+
+if [${TYPE} == "aws"]
+then
+  echo "***************************************************"
+  echo " configure aws cloud                               "
+  echo "***************************************************"
+
+  aws --version
+  if echo $? = 128 ; then
+      echo "The program 'aws' is currently not installed."
+      echo "install program 'aws'..."
+      # client aws download 
+      curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+      unzip awscliv2.zip
+      sudo ./aws/install 
+      aws --version
+      #Note of developer: is good to file ? boh! depends path and access privilage to magine. find more information.
+  else
+      echo "Continuing with aws."
+  fi
+    # get credencial access to aws cloud
+     aws configure -aws_access_key_id=${ID_KEY_ACCESSS} -aws_secret_access_key=${ID_KEY_SECRET} && clear
+  
+  echo "***************************************************"
+  echo "configure complete"
+  echo "***************************************************"
+elif [${TYPE} == "azure"] then
+  echo "***************************************************"
+  echo "configure azure cloud isn't implement"
+  echo "***************************************************"
+  exit -1
+elif [${TYPE} == "google"] then 
+  echo "***************************************************"
+  echo "configure google cloud isn't implement"
+  echo "***************************************************"
+  exit -1
+else
+  echo "***************************************************"
+  echo "configure cloud credentials isn't correct"
+  echo "***************************************************"
+  exit -1
+fi
--- .github/action/utils/integration-tests.sh
+++ .github/action/utils/integration-tests.sh
@@ -3,10 +3,32 @@
 # *************       by software Engineering             *************
 # *********************************************************************
 ACTIVE = %1
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+WORKSPACE = %2
+VERSION = "5.6.0"
+
+echo "**************************************************"
 echo "Integration tests executing"
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "**************************************************"
+
 if [${ ACTIVE }==true] then
-  cd ${{ github.workspace }}/.github/SoapUI-5.6.0/bin
-  ./testrunner.sh -a -r ${{ github.workspace }}/integration-tests/*.xml
+  echo "**************************************************"
+  echo "Getting SoapUI ${VERSION}..."
+  echo "**************************************************"
+  
+  wget http://s3.amazonaws.com/downloads.eviware/soapuios/${VERSION}/SoapUI-${VERSION}-linux-bin.tar.gz -O ${WORKSPACE}/.github/SoapUI-${VERSION}-linux-bin.tar.gz
+  cd ${WORKSPACE}/.github
+  tar -zxvf SoapUI-${VERSION}-linux-bin.tar.gz
+  rm SoapUI-${VERSION}-linux-bin.tar.gz
+ 
+  echo "**************************************************"
+  echo "find test..."  
+  echo "**************************************************"
+
+  # find file xml soapui with define endpoint.
+  cd ${WORKSPACE}/.github/SoapUI-${VERSION}/bin 
+  ./testrunner.sh -a -r ${WORKSPACE}/integration-tests/*.xml # Note of developer: may be it have more file, find all or select one. discution progress...
+  
+  echo "**************************************************"
+  echo "complete test runner..." 
+  echo "**************************************************"
 if
