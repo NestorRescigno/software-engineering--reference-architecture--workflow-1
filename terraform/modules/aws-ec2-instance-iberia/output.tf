9efa80c104a54f3fc8eeb5391547f528cd344716
NestorDevOps <nrescign@outlook.com>
2022-04-21 21:36:20 +0200

    clean

--- terraform/modules/aws-ec2-instance-iberia/output.tf
+++ terraform/modules/aws-ec2-instance-iberia/output.tf
@@ -12,3 +12,8 @@
 output "instance_id" {
   value = aws_instance.app.id
 }
+
+# NOTE OF DEVELOPERS: BOH! private or public, dns is necesary for test app.
+output "instance_ip_addr" {
+  value = aws_instance.app.private_dns
+}
