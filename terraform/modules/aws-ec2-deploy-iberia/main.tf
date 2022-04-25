2fbff2b189921f29ad77cec30274bfe917fb0b31
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-25 08:48:59 +0200

    Update main.tf

--- terraform/modules/aws-ec2-deploy-iberia/main.tf
+++ terraform/modules/aws-ec2-deploy-iberia/main.tf
@@ -3,6 +3,33 @@
 # *************       by Software Engineering             *************
 # *********************************************************************
 
+# ##############################
+# ## IAM Instance profile Role
+# ##############################
+
+resource "aws_iam_instance_profile" "service" {
+  name = "${var.service_name}-instanceprofile-${var.environment_prefix}"
+  role = aws_iam_role.service.name
+}
+
+##########################################################
+# Attach the policy cloudwatch_agent to the role
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
+  role       = aws_iam_role.service.name
+  policy_arn = data.aws_iam_policy.cloudwatch_agent.arn
+}
+
+# Attach the policy ssm to the role
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.service.name
+  policy_arn = data.aws_iam_policy.ssm.arn
+}
+
+# Attach the policy common-microservices to the role
+resource "aws_iam_role_policy_attachment" "common-microservices" {
+  role       = aws_iam_role.service.name
+  policy_arn = data.aws_iam_policy.common-microservices.arn
 
 ################################
 ### New Resource launch Template
