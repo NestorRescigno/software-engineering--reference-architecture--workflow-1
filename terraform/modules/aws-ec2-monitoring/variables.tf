98675692a90b66d602bcbc06e8150a0201bf0d96
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-25 18:21:55 +0200

    Update variables.tf

--- terraform/modules/aws-ec2-monitoring/variables.tf
+++ terraform/modules/aws-ec2-monitoring/variables.tf
@@ -2,4 +2,19 @@
 # *************           IBERIA L.A.E.                   *************
 # *************       by Software Engineering             *************
 # *********************************************************************
-# 000 - Global variable
+# setting variable
+
+
+# user monitoring alert  infrastructure example: aws_autoscaling_group.bar.name
+variable "aws_autoscaling_group_name" {
+  description = "The name of autoscaling group associated metric"
+}
+
+# The arn policy to execute actions example: aws_autoscaling_policy.bat.arn
+variable "aws_autoscaling_policy_arn" {
+  description = "The name of autoscaling group associated metric"
+}
+
+
+
+
