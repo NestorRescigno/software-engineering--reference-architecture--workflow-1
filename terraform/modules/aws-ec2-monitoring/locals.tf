122c57dac43c15ea8e541364f774aa82019c6514
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-25 19:09:04 +0200

    Update locals.tf

--- terraform/modules/aws-ec2-monitoring/locals.tf
+++ terraform/modules/aws-ec2-monitoring/locals.tf
@@ -22,30 +22,6 @@ locals {
   }
 }
 
-# *********************************************************************
-# *************           IBERIA L.A.E.                   *************
-# *************       by Software Engineering             *************
-# *********************************************************************
-# 000 - local variables
-
-###############################
-### Stack local variables
-###############################
-
-locals {
-
-  global_common_tags = {
-    Project                  = "${var.project}"
-    Environment              = "${var.environment}"
-    Name                     = "${var.service_name}"
-    Cluster                  = "${local.cluster_name}"
-    Side                     = "${var.service_name}"
-    #Type                     = "Service" //This tag is checked in deploys by jenkins, so it no longer makes sense.
-    "Application:ArtifactId" =  join("-",[var.service_name,"core"])
-    "tf:Used"                = "True"
-  }
-}
-
 ## Change these values if neccesary
 locals {
 
