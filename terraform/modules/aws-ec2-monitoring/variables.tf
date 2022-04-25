3967e9ea1028d83a040f602edc960f6b93c26939
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-25 18:23:31 +0200

    Update variables.tf

--- terraform/modules/aws-ec2-monitoring/variables.tf
+++ terraform/modules/aws-ec2-monitoring/variables.tf
@@ -4,6 +4,36 @@
 # *********************************************************************
 # setting variable
 
+# 000 - Global variables
+
+variable "aws_region" {
+  description = "Region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "project" {
+  description = "Project"
+  type        = string
+}
+
+variable "service_name" {
+  description = "The name of the service to be created"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+  default     = "development"
+}
+
+variable "environment_prefix" {
+  description = "Environment Prefix."
+  type        = string
+  default     = "dev"
+}
+
 
 # user monitoring alert  infrastructure example: aws_autoscaling_group.bar.name
 variable "aws_autoscaling_group_name" {
