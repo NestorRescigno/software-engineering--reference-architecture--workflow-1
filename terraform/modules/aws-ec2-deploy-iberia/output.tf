4a5949e02cfa4143d302f33c7a656a13a28203fb
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-25 18:33:04 +0200

    Update output.tf

--- terraform/modules/aws-ec2-deploy-iberia/output.tf
+++ terraform/modules/aws-ec2-deploy-iberia/output.tf
@@ -6,7 +6,12 @@
 output "aws_alb_target_group_arn" {
   value = var.aws_alb_target_group_arn
 }
+
 output "aws_autoscaling_group_name" {
   value = aws_autoscaling_group.asg.name
+}
+
+output "aws_autoscaling_policy_arn_requests" {
+  value = aws_autoscaling_policy.requests.arn
 }
 
