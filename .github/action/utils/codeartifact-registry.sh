540be68cef77acb06202ceded5f4ff3ef7eaeedf
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-27 14:22:36 +0200

    Update codeartifact-registry.sh

--- .github/action/utils/codeartifact-registry.sh
+++ .github/action/utils/codeartifact-registry.sh
@@ -2,10 +2,19 @@
 # *************           IBERIA L.A.E.                   *************
 # *************       by software Engineering             *************
 # *********************************************************************
-# set variable
-project = $1
-repository = $2
-format = $3
+
+# setting variable
+LANGUAGE            = ${{ env.LANGUAGE }} 
+REF                 = ${{ github.ref }} 
+GROUPID             = ${{ env.GROUP }}
+ARTIFACTID          = ${{ env.ARTIFACT  }} 
+VERSION             = ${{ env.VERSION }}
+PACKAGE-TYPE        = ${{ env.PACKAGE }}
+
+# setting contants
+PATH-SNAPSHOTS      = "/repository/snapshots/"
+PATH-RELEASE        = "/repository/releases/"
+PATH-NPM-PRIVATE    = "/npm-private/release/"
 
 # create domain with name product/project
 # aws codeartifact create-domain --domain ${{project}}
@@ -32,3 +41,9 @@ format = $3
 #   --asset guava-27.1-jre.jar \
 #   guava-27.1-jre.jar
 
+
+## use curl 
+# GET /v1/package/version/asset?asset=asset&domain=domain&domain-owner=domainOwner&format=format&namespace=namespace&package=package&repository=repository&revision=packageVersionRevision&version=packageVersion HTTP/1.1
+# or 
+# GET PUBLISH 
+# curl --request PUT https://my_domain-111122223333.d.codeartifact.us-west-2.amazonaws.com/maven/my_repo/com/mycompany/app/my-app/1.0/my-app-1.0.jar --user "aws:$CODEARTIFACT_AUTH_TOKEN" --header "Content-Type: application/octet-stream" --data-binary @target/my-app-1.0.jar
