5656fe7fd89f0b678c8ea0c7b4fef4ab1099939e
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-27 09:41:21 +0200

    Update build-instance.sh

--- .github/action/utils/build-instance.sh
+++ .github/action/utils/build-instance.sh
@@ -51,7 +51,7 @@ echo "***************************************************"
 
 if [${aws-profile} != "" ] then
      echo "****************************************"
-     echo "**    profile connect: ${aws-profile}       *"
+     echo "**  profile connect: ${aws-profile}   **"
      echo "****************************************"
      export AWS_PROFILE= ${aws-profile}
 else
