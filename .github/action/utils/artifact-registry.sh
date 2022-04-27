bbbc0b3787234af3d9c2fb80560e768611d265c6
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-04-27 15:26:59 +0200

    Update artifact-registry.sh

--- .github/action/utils/artifact-registry.sh
+++ .github/action/utils/artifact-registry.sh
@@ -4,67 +4,74 @@
 # *********************************************************************
 . setup.sh
 
-if [${REPOSITORY_URL} != ""] then
-    # setting credencials
+# setting credencials 
+if [ ${env.CODEARTIFACT}}==false ] then
+
     REPOSITORY_USER   = ${{ env.REPOSITORY_USER }}
     REPOSITORY_SECRET = ${{ env.REPOSITORY_SECRET }}
     REPOSITORY_DNS    = ${{ env.REPOSITORY_DNS }}
-    REPOSITORY_URL    ="https://${REPOSITORY_USER}:${REPOSITORY_SECRET}@${REPOSITORY_DNS}"      # DNS can't content http or https, is necesary certificate 
-  
-    # setting variable
-    LANGUAGE          =${{ env.LANGUAGE }} 
-    REF               =${{ github.ref }} 
-    GROUPID           =${{ env.GROUP}}
-    ARTIFACTID        =${{ env.ARTIFACT}} 
-    VERSION           =${{ env.VERSION}}
-    PACKAGE-TYPE      =${{ env.PACKAGE}}
-    # setting contants
-    PATH-SNAPSHOTS      = "/repository/snapshots/"
-    PATH-RELEASE        = "/repository/releases/"
-    PATH-NPM-PRIVATE    = "/npm-private/release/" 
     
-    echo "***************************************************"
-    echo "Registy artifact to nexus repository"
-    echo "***************************************************"
+else 
 
-    if [${LANGUAGE} == "java"] then
-        echo "***************************************************"
-        echo "artifact type java"
-        echo "***************************************************"
+    export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain ${{ env.PROJECT }} --domain-owner ${{ env.REPOSITORY_USER }} --query authorizationToken --output text`
+    REPOSITORY_USER   = "aws"
+    REPOSITORY_SECRET = ${{ CODEARTIFACT_AUTH_TOKEN }}
+    REPOSITORY_DNS    = ${{ env.REPOSITORY_DNS }}
+    
+fi
+    
+REPOSITORY_URL    ="https://${REPOSITORY_USER}:${REPOSITORY_SECRET}@${REPOSITORY_DNS}"      # DNS can't content http or https, is necesary certificate 
+  
+# setting variable
+LANGUAGE          =${{ env.LANGUAGE }} 
+REF               =${{ github.ref }} 
+GROUPID           =${{ env.GROUP}}
+ARTIFACTID        =${{ env.ARTIFACT}} 
+VERSION           =${{ env.VERSION}}
+PACKAGE-TYPE      =${{ env.PACKAGE}}
+# setting contants
+PATH-SNAPSHOTS      = "/repository/snapshots/"
+PATH-RELEASE        = "/repository/releases/"
+PATH-NPM-PRIVATE    = "/npm-private/release/" 
+    
+echo "***************************************************"
+echo "Registy artifact to nexus repository"
+echo "***************************************************"
+
+if [${LANGUAGE} == "java"] then
+    echo "***************************************************"
+    echo "artifact type java"
+    echo "***************************************************"
         
-        if [ ${ startsWith(${ REF }, 'refs/heads/develop') } == true ] then
-            echo "***************************************************"
-            echo "upload snapshop"
-            echo "***************************************************"
+    if [ ${ startsWith(${ REF }, 'refs/heads/develop') } == true ] then
+        echo "***************************************************"
+        echo "upload snapshop"
+        echo "***************************************************"
             
-            SNAPSHOTS_REPOSITORY_URL = ${ REPOSITORY_URL } + ${PATH-SNAPSHOTS}
-            # --batch-mode
-            # example deploy file with maven
-            mvn deploy:deploy-file 
+        SNAPSHOTS_REPOSITORY_URL = ${ REPOSITORY_URL } + ${PATH-SNAPSHOTS} # --batch-mode
+        # example deploy file with maven
+        mvn deploy:deploy-file 
             -DgroupId=${GROUPID}
             -DartifactId=${ARTIFACTID} 
             -Dversion=${VERSION}
             -DgeneratePom=true 
             -Dpackaging=${PACKAGE-TYPE} 
-            # -DrepositoryId=nexus 
+             # -DrepositoryId=nexus 
             -Durl=${ SNAPSHOTS_REPOSITORY_URL }
             -Dfile=target/${ARTIFACTID}-${VERSION}.${PACKAGE-TYPE}
             
-            echo "::set-output name=registry-repository-id::$(echo ${PATH-SNAPSHOTS})" 
-            echo "***************************************************"
-            echo "upload complete"
-            echo "***************************************************"
-        elif [${ startsWith(${ ref }, 'refs/heads/main') } == true ] then
-            echo "***************************************************"
-            echo "upload release"
-            echo "***************************************************"
+        echo "::set-output name=registry-repository-id::$(echo ${PATH-SNAPSHOTS})" 
+        echo "***************************************************"
+        echo "upload complete"
+        echo "***************************************************"
+    elif [${ startsWith(${ ref }, 'refs/heads/main') } == true ] then
+        echo "***************************************************"
+        echo "upload release"
+        echo "***************************************************"
             
-            RELEASE_REPOSITORY_URL = ${ REPOSITORY_URL } + ${PATH-RELEASE}
-            # mvn deploy -DaltSnapshotDeploymentRepository=ibis-release::default::${ RELEASE_REPOSITORY_URL } 
-            # --batch-mode
-            
-            # example deploy file with maven
-            mvn deploy:deploy-file 
+        RELEASE_REPOSITORY_URL = ${ REPOSITORY_URL } + ${PATH-RELEASE}
+        # example deploy file with maven
+        mvn deploy:deploy-file 
             -DgroupId=${GROUPID}
             -DartifactId=${ARTIFACTID} 
             -Dversion=${VERSION}
@@ -74,18 +81,17 @@ if [${REPOSITORY_URL} != ""] then
             -Durl=${ RELEASE_REPOSITORY_URL }
             -Dfile=target/${ARTIFACTID}-${VERSION}.${PACKAGE-TYPE}
             
-            echo "::set-output name=registry-repository-id::$(echo ${PATH-RELEASE})" 
-            echo "***************************************************"
-            echo "upload complete"
-            echo "***************************************************"
-        fi
-   elif [${LANGUAGE} == "angular"] then
-   
-      echo "***************************************************"
-      echo "Artifact type angular"
-      echo "***************************************************"
+        echo "::set-output name=registry-repository-id::$(echo ${PATH-RELEASE})" 
+        echo "***************************************************"
+        echo "upload complete"
+        echo "***************************************************"
+    fi
+elif [${LANGUAGE} == "angular"] then
+    echo "***************************************************"
+    echo "Artifact type angular"
+    echo "***************************************************"
       
-      if [${ startsWith(${ REF }, 'refs/heads/main') } == true ] then
+    if [${ startsWith(${ REF }, 'refs/heads/main') } == true ] then
    
         echo "***************************************************"
         echo "upload npm private release"
@@ -111,19 +117,13 @@ if [${REPOSITORY_URL} != ""] then
         echo "upload complete"
         echo "***************************************************"
       fi
-   else
-   
-      echo "***************************************************"
-      echo "Error lenguage select, options [java, angular]"
-      echo "***************************************************"
-      exit -1
-      
-   fi
-   echo "***************************************************"
-   echo "Registy complete"
-   echo "***************************************************"
 else
-  echo "***************************************************"
-  echo "Registy disable"
-  echo "***************************************************"
-fi  
+   
+    echo "***************************************************"
+    echo "Error lenguage select, options [java, angular]"
+    echo "***************************************************"
+    exit -1
+fi
+echo "***************************************************"
+echo "Registy complete"
+echo "***************************************************"
