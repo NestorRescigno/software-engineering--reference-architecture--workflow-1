bf661f9a8da562c552b9feaa569349e1e800d7f5
NestorDevOps <nrescign@outlook.com>
2022-05-03 22:08:00 +0200

    error code type and add project name inputs

--- scripts/artifact-registry.sh
+++ scripts/artifact-registry.sh
@@ -11,12 +11,12 @@ if [ $CODEARTIFACT==false ] ; then
     REPOSITORY_SECRET=$REPOSITORY_SECRET   
 else 
     REPOSITORY_OWNER=$REPOSITORY_USER
-    export CODEARTIFACT_AUTH_TOKEN='aws codeartifact get-authorization-token --domain $PROJECT --domain-owner $REPOSITORY_OWNER --query authorizationToken --output text'
+    export CODEARTIFACT_AUTH_TOKEN='aws codeartifact get-authorization-token --domain ${PROJECT} --domain-owner ${REPOSITORY_OWNER} --query authorizationToken --output text'
     REPOSITORY_USER='aws'
     REPOSITORY_SECRET=$CODEARTIFACT_AUTH_TOKEN 
 fi
 REPOSITORY_DNS=$REPOSITORY_DNS    
-REPOSITORY_URL="https://${REPOSITORY_USER}:${REPOSITORY_SECRET}@${REPOSITORY_DNS}"      # DNS can't content http or https, is necesary certificate 
+REPOSITORY_URL='https://${REPOSITORY_USER}:${REPOSITORY_SECRET}@${REPOSITORY_DNS}'      # DNS can't content http or https, is necesary certificate 
 
 # return user and token access to repository because token auto generate in this jobs, phase build image download form codeartifact or nexus.
 echo "::set-output name=registry-repository-owner::$(echo ${REPOSITORY_OWNER})" 
