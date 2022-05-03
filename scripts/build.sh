c8b83b1db1fbb1b423498c1ca34e773437fc6134
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-05-03 18:50:47 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@
 # ${{ env.SCRIPT }}/build.sh ${{ github.workspace }} ${{ env.LANGUAGE }} ${{ github.ref }}
 sh ./setup.sh
  
-workspace = ${{ github.workspace }}
+workspace = $1
 lenguage = ${{ env.LANGUAGE }}
 ref = ${{ github.ref }}
 
