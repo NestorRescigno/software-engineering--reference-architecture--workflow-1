dc3e63e7ea71d1afa0b830220383a727ab1b9658
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-05-04 09:55:35 +0200

    Update artifact-registry.sh

--- scripts/artifact-registry.sh
+++ scripts/artifact-registry.sh
@@ -50,7 +50,7 @@ if [ $LANGUAGE=="java" ] ; then
             
         SNAPSHOTS_REPOSITORY_URL=${REPOSITORY_URL}+${PATH_SNAPSHOTS} # --batch-mode
         # example deploy file with maven
-        mvn deploy:deploy-file -DgroupId=${GROUPID} -DartifactId=${ARTIFACTID} -Dversion=${VERSION} -DgeneratePom=true -Dpackaging=${PACKAGE_TYPE} -Durl=${ SNAPSHOTS_REPOSITORY_URL } -Dfile=target/${ARTIFACTID}-${VERSION}.${PACKAGE_TYPE}
+        mvn deploy:deploy-file -DgroupId=${GROUPID} -DartifactId=${ARTIFACTID} -Dversion=${VERSION} -DgeneratePom=true -Dpackaging=${PACKAGE_TYPE} -Durl=${SNAPSHOTS_REPOSITORY_URL} -Dfile=target/${ARTIFACTID}-${VERSION}.${PACKAGE_TYPE}
             # -DrepositoryId=nexus \
             
         echo "::set-output name=registry-repository-id::$(echo ${PATH_SNAPSHOTS})" 
