2a9a9685e0c19505618886df3e4deee88a224e7f
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-05-04 13:42:36 +0200

    Update artifact-registry.sh

--- scripts/artifact-registry.sh
+++ scripts/artifact-registry.sh
@@ -5,7 +5,7 @@
 # *********************************************************************
 # sh ./setup.sh
 # setting credencials 
-if [ $CODEARTIFACT=false ] ; then
+if [ $CODEARTIFACT ] ; then
     echo "entro por nexus"
     REPOSITORY_USER=$REPOSITORY_USER
     REPOSITORY_SECRET=$REPOSITORY_SECRET   
