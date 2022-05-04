bc11c3c8c889f709044828bb23ec06cec56d8dda
NestorRescigno <100286341+NestorRescigno@users.noreply.github.com>
2022-05-04 14:39:28 +0200

    Update artifact-registry.sh

--- scripts/artifact-registry.sh
+++ scripts/artifact-registry.sh
@@ -60,6 +60,7 @@ if [ $LANGUAGE=="java" ] ; then
             
         SNAPSHOTS_REPOSITORY_URL="${REPOSITORY_URL}${PATH_SNAPSHOTS}" # --batch-mode
         # example deploy file with maven
+        ls
         mvn deploy:deploy-file -DgroupId=$GROUPID -DartifactId=$ARTIFACTID -Dversion=$VERSION -DgeneratePom=true -Dpackaging=$PACKAGE_TYPE -Dfile=target/$ARTIFACTID-$VERSION.$PACKAGE_TYPE -Durl=$SNAPSHOTS_REPOSITORY_URL --settings $WORKSPACE/.github/cicd/scripts/settings.xml
         
         # -DrepositoryId=nexus \  
@@ -74,6 +75,7 @@ if [ $LANGUAGE=="java" ] ; then
         echo "***************************************************"
             
         RELEASE_REPOSITORY_URL="${REPOSITORY_URL}${PATH_RELEASE}"
+        ls
         # example deploy file with maven
         mvn deploy:deploy-file -DgroupId=$GROUPID -DartifactId=$ARTIFACTID -Dversion=$VERSION -DgeneratePom=true -Dpackaging=$PACKAGE_TYPE -Dfile=target/$ARTIFACTID-$VERSION.$PACKAGE_TYPE -DrepositoryId=codeartifact -Durl=$RELEASE_REPOSITORY_URL --settings $WORKSPACE/.github/cicd/scripts/settings.xml 
             
