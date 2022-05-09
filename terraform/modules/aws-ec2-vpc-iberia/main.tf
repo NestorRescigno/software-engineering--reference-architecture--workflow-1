f83123f67368a47df6f93442f7b2f155c505959c
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-09 11:30:07 +0200

    create vpc in terraform

--- terraform/modules/aws-ec2-vpc-iberia/main.tf
+++ terraform/modules/aws-ec2-vpc-iberia/main.tf
@@ -279,3 +279,20 @@ resource "aws_route53_record" "green-record" {
 }
 
 # NOTE OF DEVELOPERS :resource module VPC and subnet (I fail! remove!!) 
+# create vitual private cloud network for product
+resource "aws_vpc" "vpc_product" {
+    # The IPv4 CIDR block for the VPC. CIDR can be explicitly set or it can be derived from IPAM using
+  cidr_block       = "10.0.0.0/16"
+  # A tenancy option for instances launched into the VPC. 
+  # Default is default, which ensures that EC2 instances launched
+  # in this VPC use the EC2 instance tenancy attribute specified 
+  # when the EC2 instance is launched. The only other option is dedicated, 
+  # which ensures that EC2 instances launched in this VPC are run on dedicated 
+  # tenancy instances regardless of the tenancy attribute specified at launch. 
+  # This has a dedicated per region fee of $2 per hour, plus an hourly per instance usage fee.
+  instance_tenancy = "default"
+  # A map of tags to assign to the resource. If configured with a provider
+  tags = {
+    Name = "${var.project}-${var.environment}"
+  }
+}
