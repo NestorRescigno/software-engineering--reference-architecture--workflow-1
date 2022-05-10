fb44009e9652d9e32071d15d7d166b0503ff4666
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-10 09:19:17 +0200

    add path relative to module terraform in workspace

--- scripts/build-instance.sh
+++ scripts/build-instance.sh
@@ -56,7 +56,7 @@ fi
 ##################################
 
 # init terraform module
-cd ${WORKSPACE}/terraform/modules/aws-ec2-instance-iberia
+cd ${WORKSPACE}/.github/cicd/terraform/modules/aws-ec2-instance-iberia
 terraform init
 # create plan terrafom
 terraform plan -var "lenguage_code=${LANGUAGE}" -var "instance_type=${INSTANCE_TYPE}" -var "ref=${ARTIFACTREF}" -var "package=${PACKAGE}" -var "project_name=${PROJECT}" -var "service_name=${ARTIFACT}" -var "service_version=${VERSION}" -var "artifact_user=${REPOSITORY_USER}" -var "artifact_secret=${REPOSITORY_SECRET}" 
