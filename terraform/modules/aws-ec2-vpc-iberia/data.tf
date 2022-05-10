4508cbc26ab112a2c5d6c7ea354ed18ad9e78ede
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-10 07:41:10 +0200

    remove count route

--- terraform/modules/aws-ec2-vpc-iberia/data.tf
+++ terraform/modules/aws-ec2-vpc-iberia/data.tf
@@ -29,7 +29,6 @@ data "aws_iam_instance_profile" "ip" {
 ###########################
 
 data "aws_route53_zone" "route_local" {
-  count = var.state != null ? 1 : 0 
   name         = join(".",[var.environment,var.project,var.global_dns])
   private_zone = true
 }
