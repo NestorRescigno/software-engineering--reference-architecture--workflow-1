2bcdc23292b8aa273d7e3550776b468ac0a9eea7
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-10 10:03:24 +0200

    test count index

--- terraform/modules/aws-ec2-instance-iberia/output.tf
+++ terraform/modules/aws-ec2-instance-iberia/output.tf
@@ -10,10 +10,10 @@
 
 # output id new instance
 output "instance_id" {
-  value = aws_instance.app.id
+  value = aws_instance.app[count.index].id
 }
 
 # NOTE OF DEVELOPERS: BOH! private or public, dns is necesary for test app.
 output "instance_ip_addr" {
-  value = aws_instance.app.private_dns
+  value = aws_instance.app[count.index].private_dns
 }
