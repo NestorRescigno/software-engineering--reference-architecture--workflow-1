7681de9c94f2f30e9e0dd89140ae76663f656328
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-10 18:04:16 +0200

    comment output for test

--- terraform/modules/aws-ec2-instance-iberia/output.tf
+++ terraform/modules/aws-ec2-instance-iberia/output.tf
@@ -9,11 +9,11 @@
 #}
 
 # output id new instance
-output "instance_id" {
-  value = [for s in aws_instance.app : s.id]
-}
+# output "instance_id" {
+#  value = [for s in aws_instance.app : s.id]
+#}
 
 # NOTE OF DEVELOPERS: BOH! private or public, dns is necesary for test app.
-output "instance_ip_addr" {
-  value = [for s in aws_instance.app : s.private_dns]
-}
+#output "instance_ip_addr" {
+#  value = [for s in aws_instance.app : s.private_dns]
+#}
