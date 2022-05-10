cc3f1a72222ba6c5aab4cfee288a4d84ba8d2423
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-10 15:16:46 +0200

    test

--- terraform/modules/aws-ec2-instance-iberia/output.tf
+++ terraform/modules/aws-ec2-instance-iberia/output.tf
@@ -10,10 +10,10 @@
 
 # output id new instance
 output "instance_id" {
-  value = aws_instance.app[0].id
+  value = aws_instance.app.id
 }
 
 # NOTE OF DEVELOPERS: BOH! private or public, dns is necesary for test app.
 output "instance_ip_addr" {
-  value = aws_instance.app[0].private_dns
+  value = aws_instance.app.private_dns
 }
