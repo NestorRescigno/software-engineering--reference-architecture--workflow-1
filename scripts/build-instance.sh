4fd4b0f92103329cdfd220675ddb2594887a021c
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-11 14:26:38 +0200

    add exit code

--- scripts/build-instance.sh
+++ scripts/build-instance.sh
@@ -64,8 +64,14 @@ fi
 
 terraform init
 # create plan terrafom
-terraform apply --detailed-exitcode -auto-approve -var "lenguage_code=${LENGUAGE}" -var "instance_type=${INSTANCE_TYPE}" -var "ref=${ARTIFACTREF}" -var "package=${PACKAGE}" -var "project=${PROJECT}" -var "service_name=${ARTIFACT}" -var "service_version=${VERSION}" -var "service_groupid=${GROUP}" -var "artifact_user=${REPOSITORY_USER}" -var "artifact_secret=${REPOSITORY_SECRET}"  -var "environment=${ENVIROMENT_DEV}" -var "environment_prefix=${ENVIROMENT_PREFIX_DEV}"
-echo $?
+terraform apply -auto-approve -var "lenguage_code=${LENGUAGE}" -var "instance_type=${INSTANCE_TYPE}" -var "ref=${ARTIFACTREF}" -var "package=${PACKAGE}" -var "project=${PROJECT}" -var "service_name=${ARTIFACT}" -var "service_version=${VERSION}" -var "service_groupid=${GROUP}" -var "artifact_user=${REPOSITORY_USER}" -var "artifact_secret=${REPOSITORY_SECRET}"  -var "environment=${ENVIROMENT_DEV}" -var "environment_prefix=${ENVIROMENT_PREFIX_DEV}"
+if [ echo $? == 1 ] ; then 
+   echo "***************************************************"
+   echo " Error terrafom apply resource "
+   echo " stop workflow progess... "
+   echo "***************************************************"
+   exit -1
+fi
 
 #apply plan terrafom
 # terraform apply # temporal comment to test
