41bbd7652efbd50c585da5a9824881d624289c6e
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-12 16:51:21 +0200

    test

--- terraform/modules/aws-ec2-profile-iberia/main.tf
+++ terraform/modules/aws-ec2-profile-iberia/main.tf
@@ -24,6 +24,7 @@ provider "aws" {
 }
 
 
+
 ############################
 ### New instance profile 
 ############################
@@ -34,148 +35,107 @@ resource "aws_iam_role" "role" {
   name = join("-", [var.project, var.environment, "role"])
   path = "/"
   
-  #managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+  # managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
   
   # asssigne standars policy
   assume_role_policy = <<EOF
   {
-      "Version": "2012-10-17",
-      "Statement": [
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "ssm:DescribeAssociation",
-                  "ssm:GetDeployablePatchSnapshotForInstance",
-                  "ssm:GetDocument",
-                  "ssm:DescribeDocument",
-                  "ssm:GetManifest",
-                  "ssm:GetParameters",
-                  "ssm:ListAssociations",
-                  "ssm:ListInstanceAssociations",
-                  "ssm:PutInventory",
-                  "ssm:PutComplianceItems",
-                  "ssm:PutConfigurePackageResult",
-                  "ssm:UpdateAssociationStatus",
-                  "ssm:UpdateInstanceAssociationStatus",
-                  "ssm:UpdateInstanceInformation"
-              ],
-              "Resource": "*"
-          },
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "ssmmessages:CreateControlChannel",
-                  "ssmmessages:CreateDataChannel",
-                  "ssmmessages:OpenControlChannel",
-                  "ssmmessages:OpenDataChannel"
-              ],
-              "Resource": "*"
-          },
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "ec2messages:AcknowledgeMessage",
-                  "ec2messages:DeleteMessage",
-                  "ec2messages:FailMessage",
-                  "ec2messages:GetEndpoint",
-                  "ec2messages:GetMessages",
-                  "ec2messages:SendReply"
-              ],
-              "Resource": "*"
-          },
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "cloudwatch:PutMetricData"
-              ],
-              "Resource": "*"
-          },
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "ec2:DescribeInstanceStatus"
-              ],
-              "Resource": "*"
-          },
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "ds:CreateComputer",
-                  "ds:DescribeDirectories"
-              ],
-              "Resource": "*"
-          },
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "logs:CreateLogGroup",
-                  "logs:CreateLogStream",
-                  "logs:DescribeLogGroups",
-                  "logs:DescribeLogStreams",
-                  "logs:PutLogEvents"
-              ],
-              "Resource": "*"
-          },
-          {
-              "Effect": "Allow",
-              "Action": [
-                  "s3:GetBucketLocation",
-                  "s3:PutObject",
-                  "s3:GetObject",
-                  "s3:GetEncryptionConfiguration",
-                  "s3:AbortMultipartUpload",
-                  "s3:ListMultipartUploadParts",
-                  "s3:ListBucket",
-                  "s3:ListBucketMultipartUploads"
-              ],
-              "Resource": "*"
-          }
-      ]
-  } 
-  EOF
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:DescribeAssociation",
+                "ssm:GetDeployablePatchSnapshotForInstance",
+                "ssm:GetDocument",
+                "ssm:DescribeDocument",
+                "ssm:GetManifest",
+                "ssm:GetParameters",
+                "ssm:ListAssociations",
+                "ssm:ListInstanceAssociations",
+                "ssm:PutInventory",
+                "ssm:PutComplianceItems",
+                "ssm:PutConfigurePackageResult",
+                "ssm:UpdateAssociationStatus",
+                "ssm:UpdateInstanceAssociationStatus",
+                "ssm:UpdateInstanceInformation"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssmmessages:CreateControlChannel",
+                "ssmmessages:CreateDataChannel",
+                "ssmmessages:OpenControlChannel",
+                "ssmmessages:OpenDataChannel"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2messages:AcknowledgeMessage",
+                "ec2messages:DeleteMessage",
+                "ec2messages:FailMessage",
+                "ec2messages:GetEndpoint",
+                "ec2messages:GetMessages",
+                "ec2messages:SendReply"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:PutMetricData"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeInstanceStatus"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ds:CreateComputer",
+                "ds:DescribeDirectories"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetBucketLocation",
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:GetEncryptionConfiguration",
+                "s3:AbortMultipartUpload",
+                "s3:ListMultipartUploadParts",
+                "s3:ListBucket",
+                "s3:ListBucketMultipartUploads"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
 
 }
-
-
-
-
-# resource "aws_iam_role" "example" {
-#   name                = "yak_role"
-#   assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
-#   managed_policy_arns = [aws_iam_policy.policy_one.arn, aws_iam_policy.policy_two.arn]
-# }
-
-# resource "aws_iam_policy" "policy_one" {
-#   name = "policy-618033"
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action   = ["ec2:Describe*"]
-#         Effect   = "Allow"
-#         Resource = "*"
-#       },
-#     ]
-#   })
-# }
-
-# resource "aws_iam_policy" "policy_two" {
-#   name = "policy-381966"
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action   = ["s3:ListAllMyBuckets", "s3:ListBucket", "s3:HeadBucket"]
-#         Effect   = "Allow"
-#         Resource = "*"
-#       },
-#     ]
-#   })
-# }
-
-
 
 # create instance profile
 resource "aws_iam_instance_profile" "iam_instance_profile" {
