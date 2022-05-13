2da0deb79aa9309f44d08fa79384ef45b00f4a96
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-13 13:16:05 +0200

    test connect load balancer to instance

--- terraform/modules/aws-ec2-securitygroup-iberia/main.tf
+++ terraform/modules/aws-ec2-securitygroup-iberia/main.tf
@@ -100,7 +100,6 @@ resource "aws_security_group" "instances" {
     from_port       = 8080
     to_port         = 8080 
     protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
     description = "From ${var.service_name} ALB"
   }
 
