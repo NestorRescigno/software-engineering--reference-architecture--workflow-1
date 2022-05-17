dcf3a7379ef3dd2c2d31c0168718891f6463372d
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-17 07:33:17 +0200

    add alb to instance and creare resource

--- terraform/modules/aws-ec2-securitygroup-iberia/main.tf
+++ terraform/modules/aws-ec2-securitygroup-iberia/main.tf
@@ -24,6 +24,48 @@ provider "aws" {
 
 
 #####################################
+### New commons resource
+######################################
+
+# commons service access to-from microservice
+resource "aws_security_group" "sg-commons-microservices" {
+  # asign name: demo-alb-dev-sg
+  name        = join("-",[var.project, var.environment_prefix, "sg", "common", "microservice"])
+  description = "SG for common access to-from microservices"
+
+  ## outbound all traffic
+  egress {
+    from_port      = 0
+    to_port        = 0
+    protocol       = "-1"
+  
+     # If your requirement is to allow all the traffic from internet you can use
+    cidr_blocks      = ["0.0.0.0/0"] 
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
+
+
+# commons service access to-from microservice
+resource "aws_security_group" "sg-commons-microservices-alb" {
+  # asign name: demo-alb-dev-sg
+  name        = join("-", [var.project, var.environment_prefix, "sg", "common", "microservice","alb"])
+  description = "SG for common access to-from microservices ALBs"
+
+  ## outbound all traffic
+  egress {
+    from_port      = 8080
+    to_port        = 8080
+    protocol       = "tpc"
+    cidr_blocks    = []
+  }
+
+ 
+}
+
+
+#####################################
 ### New resources ALB SG 
 ######################################
 
@@ -108,7 +150,6 @@ resource "aws_security_group" "instances" {
     from_port      = 0
     to_port        = 0
     protocol       = "-1"
-  
      # If your requirement is to allow all the traffic from internet you can use
     cidr_blocks      = ["0.0.0.0/0"] 
     ipv6_cidr_blocks = ["::/0"]
@@ -123,3 +164,66 @@ resource "aws_security_group" "instances" {
 
 }
 
+# #################################
+# ### add endpoint
+# #################################
+
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id            = data.aws_vpc.vpc_product.id
+  service_name      = join(".",["com","amazonaws",var.aws_region, "ec2"])
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    data.aws_security_group.instances.id
+  ]
+
+  private_dns_enabled = true
+}
+
+# resource "aws_vpc_endpoint" "s3" {
+#   vpc_id            = data.aws_vpc.vpc_product.id
+#   service_name      = join(".",["com","amazonaws",var.aws_region, "s3"])
+#   vpc_endpoint_type = "Interface"
+
+#   security_group_ids = [
+#     data.aws_security_group.alb.id, data.aws_security_group.instances.id
+#   ]
+
+#   private_dns_enabled = true
+# }
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id            = data.aws_vpc.vpc_product.id
+  service_name      =  join(".",["com","amazonaws",var.aws_region, "ec2messages"])
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    data.aws_security_group.instances.id
+  ]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = data.aws_vpc.vpc_product.id
+  service_name      = join(".",["com","amazonaws",var.aws_region, "ssm"])
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    data.aws_security_group.instances.id
+  ]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id            = data.aws_vpc.vpc_product.id
+  service_name      = join(".",["com","amazonaws",var.aws_region, "ssmmessages"])
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    data.aws_security_group.instances.id
+  ]
+
+  private_dns_enabled = true
+}
