d431ba9680026590ec50b7c2a3474b49515b06b9
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-19 17:06:45 +0200

    test nad and getway

--- terraform/modules/aws-ec2-vpc-iberia/main.tf
+++ terraform/modules/aws-ec2-vpc-iberia/main.tf
@@ -67,6 +67,10 @@ resource "aws_subnet" "subnets" {
   vpc_id            = aws_vpc.vpc_product.id
   # get key of item array
   availability_zone = each.key
+
+  # map ips public 
+  map_public_ip_on_launch = true
+
   # set IP 
   cidr_block        = cidrsubnet(aws_vpc.vpc_product.cidr_block, 4, var.az_number[each.value.name_suffix])
   # set tag
@@ -75,7 +79,69 @@ resource "aws_subnet" "subnets" {
   }
 }
 
+####################################################################################
+#create Internet Gateway
+#será necesario configurar la variable public_mask distinto de 0 para que se genere
+####################################################################################
+
+resource "aws_internet_gateway" "igw_dc" {
+  count  = length(var.public_subnets) != 0 ? 1 : 0
+  vpc_id = aws_vpc.vpc_product.id
+  #tags      = merge(var.common_tags, map("Name", "igw-${var.vpc_name}"))
+  tags = merge(var.common_tags, tomap({ "Name" = "igw-${aws_vpc.vpc_product.id}" }))
+}
 
 
+####################################################################################
+#Creación de NAT Gateway 
+#será necesario configurar la variable enable_nat_gateway a true
+####################################################################################
+
+resource "aws_eip" "nat_eip" {
+  for_each = data.aws_availability_zone.all
+  vpc   = true
+  #tags          = merge(var.common_tags, map("Name", "eip-${var.vpc_name}-${element(var.azs, count.index)}"))
+  tags = merge(var.common_tags, tomap({ "Name" = "eip-${aws_vpc.vpc_product.id}-${each.value}" }))
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  for_each = data.aws_availability_zone.all
+  count =  length(data.aws_availability_zone.all)
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id = aws_subnet.subnets.id
+  #allocation_id = element(aws_eip.nat_eip.*.id, count.index)
+  #subnet_id     = element(aws_subnet.subnet.*.id, count.index)
+  #tags          = merge(var.common_tags, map("Name", "natgw-${var.vpc_name}-${element(var.azs, count.index)}"))
+  tags = merge(var.common_tags, tomap({ "Name" = "natgw-${aws_vpc.vpc_product.id}-${each.value}" }))
+
+  depends_on = [aws_internet_gateway.igw_dc]
+}
 
 
+####################################################################################
+# route net public
+####################################################################################
+resource "aws_route_table" "rt_igw_dc" {
+  count  = length(aws_subnet.subnets) != 0 ? 1 : 0
+  vpc_id = aws_vpc.vpc_product.id
+  #tags   = merge(var.common_tags, map("Name", "rt-${var.vpc_name}-public"))
+  tags = merge(var.common_tags, tomap({ "Name" = "rt-${aws_vpc.vpc_product.id}-public" }))
+}
+
+# all traffic NO-LOCAL subnet public route to IGW-Gateway
+resource "aws_route" "rt_igw_dc" {
+  count                  = length(aws_subnet.subnets.id) != 0 ? 1 : 0
+  route_table_id         = aws_route_table.rt_igw_dc[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw_dc[0].id
+  depends_on             = [aws_internet_gateway.igw_dc]
+}
+
+# asigned route public to Internet Gateway subnet public
+resource "aws_route_table_association" "rt_igw_dc_asoc" {
+  #vpc_id = "${aws_vpc.vpc.id}"
+  count          = length(aws_subnet.subnets.id) != 0 ? length(data.aws_availability_zone.all) : 0
+  route_table_id = aws_route_table.rt_igw_dc[0].id
+  subnet_id      = aws_subnet.subnets[count.index].id
+  depends_on     = [aws_route_table.rt_igw_dc]
+}
