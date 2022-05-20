f61d5f86e60023cd0d19589ad430f0263e9396a1
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-20 09:46:53 +0200

    test

--- terraform/modules/aws-ec2-vpc-iberia/main.tf
+++ terraform/modules/aws-ec2-vpc-iberia/main.tf
@@ -177,8 +177,8 @@ resource "aws_route_table" "private" {
 
 resource "aws_route" "private_nat_gateway" {
   for_each = data.aws_availability_zone.all
-  route_table_id         = aws_route_table.private[each.key.index].id
-  nat_gateway_id         = aws_nat_gateway.nat_gateway[each.key.index].id
+  route_table_id         = aws_route_table.private[each.key].id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway[each.key].id
   destination_cidr_block = "0.0.0.0/0"
 
   timeouts {
