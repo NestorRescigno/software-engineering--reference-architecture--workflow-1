3db6eba44819c84b5444fe8a86a0875d1eaa3d11
NestorRescigno <nestor.rescigno@accenture.com>
2022-05-24 08:43:24 +0200

    test

--- terraform/modules/aws-ec2-gateway-iberia/main.tf
+++ terraform/modules/aws-ec2-gateway-iberia/main.tf
@@ -150,7 +150,7 @@ resource "aws_vpc_endpoint" "s3" {
 resource "aws_vpc_endpoint_route_table_association" "private_s3" {
   for_each = data.aws_availability_zone.all
   vpc_endpoint_id = aws_vpc_endpoint.s3.id
-  route_table_id  = aws_route_table.private[data.aws_availability_zone.all, each.key].id
+  route_table_id  = aws_route_table.private[index(data.aws_availability_zone.all, each.key)].id
 }
 
 # resource "aws_vpc_endpoint_route_table_association" "public_s3" {
